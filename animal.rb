9e1c660948f3cda3b32cd037e2cd438225eba46e
syotaro.sasaki <ssk.syotaro.0921@gmail.com>
2021-10-01 08:29:03 +0000

    animal.rb

---
+++ animal.rb
@@ -0,0 +1,14 @@
+class Animal
+    attr_accessor:name,:age
+ def initialize(name,age)
+     self.name=name
+     self.age=age
+ end
+ def say()
+     puts "#{self.name}です。#{self.age}歳です。"
+ end
+end
+
+animal=Animal.new("田中　太郎",25)
+animal=Animal.new("鈴木　次郎",30)
+animal=Animal.new("佐藤　花子",20)
